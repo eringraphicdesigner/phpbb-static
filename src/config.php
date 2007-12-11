be40715bb625ef83118e88a3ccea697ffedd4229
fajran <fajran@d254fb16-0b41-0410-bbae-d37e45f24428>
2007-12-11 23:35:57 +0000

    files

---
+++ src/config.php
@@ -0,0 +1,13 @@
+<?
+
+$db_host = '';
+$db_user = '';
+$db_pass = '';
+$db_name = '';
+
+$filter_forum = array();
+$filter_topic = array();
+
+$template_dir = 'templates/';
+$target_dir = 'static/';
+
