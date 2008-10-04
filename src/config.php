9ab7f326434d2b3d6c0a4ac49b1d95dcc8331f5c
fajran <fajran@d254fb16-0b41-0410-bbae-d37e45f24428>
2008-10-04 11:22:54 +0000

    - Add configuration variable for forum name, description, and url (issue #1) - Add support for database prefix - Fix forum pages generator so they contain forum title

--- src/config.php
+++ src/config.php
@@ -1,13 +1,18 @@
 <?
 
-$db_host = '';
-$db_user = '';
-$db_pass = '';
-$db_name = '';
+$db_host = '192.168.1.25';
+$db_user = 'target_user';
+$db_pass = 'targ3T';
+$db_name = 'forum';
+$db_prefix = 'phpbb_';
 
 $filter_forum = array();
 $filter_topic = array();
 
 $template_dir = 'templates/';
 $target_dir = 'static/';
+
+$forum_name = 'Forum @ Linux.Or.Id';
+$forum_description = 'Forum Pengguna Linux Indonesia';
+$forum_url = 'http://forum.linux.or.id/';
 
